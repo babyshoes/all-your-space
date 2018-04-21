aad6a22fb4c3f594dcb8578c43cbb60ceae506f7
babyshoes <dianecai@gmail.com>
2018-04-21 19:53:54 -0400

    both alien and predator render

--- invaders.js
+++ invaders.js
@@ -1,2 +1,94 @@
-var canvas = getElementByID('c')
-var ctx = canvas.getContext('2d')
+// TO DO:
+// - impose field boundaries
+// - load phalanx of invaders
+// spawn animation
+
+class Player {
+  constructor(color, startingX, startingY, width=40, height=40, lives=1) {
+    this.color = color
+    this.lives = lives
+    this.width = width
+    this.height = height
+    this.posX = startingX
+    this.posY = startingY
+  }
+
+  move(spacesOver) {
+    this.posX += spacesOver
+  }
+
+  shoot(){
+
+  }
+}
+
+var Field = (function() {
+	var canvas = document.getElementById('c')
+	var ctx = canvas.getContext('2d')
+  var width = canvas.width / spacing
+  var height = canvas.width / spacing
+  var playerMove = 0
+
+  var computer = new Player('red', canvas.width / 2, 10)
+  var human = new Player('white', canvas.width / 2, 300)
+
+  window.addEventListener("keydown", function (event) {
+  if (event.defaultPrevented) {
+    return;
+  }
+
+  switch (event.key) {
+    case "ArrowLeft":
+      playerMove = -8
+      break;
+    case "ArrowRight":
+      playerMove = 8
+      break;
+    case ' ':
+      // Do something for "enter" or "return" key press.
+      break;
+    default:
+      // playerMove = 0
+      return; // Quit when this doesn't handle the key event.
+  }
+
+  // Cancel the default action to avoid it being handled twice
+    event.preventDefault();
+  }, true)
+
+  window.addEventListener("keyup", function (event) {
+    switch (event.key) {
+      case "ArrowLeft":
+        playerMove = 0
+        break;
+      case "ArrowRight":
+        playerMove = 0
+        break;
+      default:
+        return; // Quit when this doesn't handle the key event.
+    }
+  })
+
+  var drawPlayer = ({color, posX, posY, width, height}) => {
+    ctx.fillStyle = color
+    ctx.fillRect(posX, posY, width, height)
+  }
+
+	function draw() {
+	  window.requestAnimationFrame(draw)
+    ctx.clearRect(0, 0, canvas.width, canvas.height)
+
+    human.move(playerMove)
+    drawPlayer({...human})
+    drawPlayer({...computer})
+	}
+
+	return {
+		animate: draw,
+    canvasWidth: canvas.width,
+    canvasHeight: canvas.height
+	}
+})()
+
+field = Field
+field.animate()
